e947ad4cff26947968621c6e200be07aa5a830f1
Ander-Villantoy <ander.villantoy@gmail.com>
2025-02-12 11:53:13 -0500

    Se cambio las imagenes a webp

--- js/projects.js
+++ js/projects.js
@@ -2,25 +2,25 @@ const proyectos = [
     {
         titulo: "Pino Escalonado",
         descripcion: `Estante hecho en madera (Pino Radiata 🌲). Desarmable para su fácil traslado, acabado en cera neutra y aroma floral.`,
-        imagen: "img/Estante de exhibición portatil - 001.png",
+        imagen: "img/Estante de exhibición portatil - 001.webp",
         enlace: "https://www.instagram.com/p/CdUvdf3Ml84/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
     },
     {
         titulo: "PortaCelulares Picasso",
         descripcion: "Accesorio de celular hecho en madera (Pino Radiata y Mohena ) 🌲 Acabado en cera neutra y aroma floral 🍃.",
-        imagen: "img/portaCelulares - Minimalista - 001.png",
+        imagen: "img/portaCelulares - Minimalista - 001.webp",
         enlace: "https://www.instagram.com/p/CcIomDWu85g/?utm_source=ig_web_copy_link"
     },
     {
         titulo: "PosaVasos Solsticio",
         descripcion: "Utensilio para vasos y tazas de Pino Radiata y Mohena 🌳 con acabado en laca para mayor protección, Diseño exclusivo 😎",
-        imagen: "img/posaVasos - Vintage - 001.png",
+        imagen: "img/posaVasos - Vintage - 001.webp",
         enlace: "https://www.instagram.com/p/CcIp07MOaN4/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
     },
     {
         titulo: "PortaCelulares Artistico",
         descripcion: "Accesorio de celular hecho en madera (Pino Radiata y Mohena ) 🌳 acabado en laca para mayor protección 💪",
-        imagen: "img/portaCelulares - Vintage - 003.png",
+        imagen: "img/portaCelulares - Vintage - 003.webp",
         enlace: "https://www.instagram.com/p/CcIqL6RuubD/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
     }
 ];
